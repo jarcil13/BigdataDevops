ad904cc7d369139124aba3704e29e303a72c3a16
Juan David Arcila Moreno <jarcil13@eafit.edu.co>
2018-10-31 18:16:38 -0500

    Adding expect setup script

---
+++ ansible/roles/ambari-master/files/ambari-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SETUP=$(expect -c "
+set timeout -1
+spawn ambari-server setup
+expect \"OK to continue [y/n] (y)?\"
+send \"y\r\"
+expect \"Customize user account for ambari-server daemon [y/n] (n)\"
+send \"n\r\"
+expect \"OK to continue [y/n] (y)?\"
+send \"y\r\"
+expect \"Enter choice (1):\"
+send \"1\r\"
+expect \"Do you accept the Oracle Binary Code License Agreement [y/n] (y)?\"
+send \"y\r\"
+expect \"Enable Ambari Server to download and install GPL Licensed LZO packages [y/n] (n)?\"
+send \"y\r\"
+expect \"Enter advanced database configuration [y/n] (n)?\"
+send \"n\r\"
+expect eof
+")
+
+echo "$SETUP"
